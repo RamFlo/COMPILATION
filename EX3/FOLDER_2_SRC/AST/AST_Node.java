c736abc0da1ca60e8012992c8c9a9db27bab238e
Ram S <ramsh92@gmail.com>
2018-12-01 19:06:45 +0200

    sdsdf

--- EX3/FOLDER_2_SRC/AST/AST_Node.java
+++ EX3/FOLDER_2_SRC/AST/AST_Node.java
@@ -8,6 +8,7 @@ public abstract class AST_Node
 	/* a graphviz dot format of the AST ...    */
 	/*******************************************/
 	public int SerialNumber;
+	public int lineNum, colNum;
 	
 	/***********************************************/
 	/* The default message for an unknown AST node */
@@ -15,5 +16,10 @@ public abstract class AST_Node
 	public void PrintMe()
 	{
 		System.out.print("AST NODE UNKNOWN\n");
+	}
+	public void setLineAndColNum(int line, int col)
+	{
+		this.lineNum = line;
+		this.colNum = col;
 	}
 }
